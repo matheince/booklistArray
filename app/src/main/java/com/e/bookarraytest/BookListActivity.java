ce134ce74a694d192548d5861f5c48be7563c1a5
WoongHui Lee <comyoung21@gmail.com>
2020-02-06 12:39:39 +0900

    북리스트 연습 - 자바코드 작성

---
+++ app/src/main/java/com/e/bookarraytest/BookListActivity.java
@@ -0,0 +1,44 @@
+package com.e.bookarraytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
+
+public class BookListActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerview_books);
+        new FirebaseDatabaseHelper().readBooks(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Book> books, List<String> keys) {
+                findViewById(R.id.loading_books_pb).setVisibility(View.GONE);
+                
+                new Recyclerview_Config().setConfig(mRecyclerView, BookListActivity.this, books, keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
+
+    }
+}
